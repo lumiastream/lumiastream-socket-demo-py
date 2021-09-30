ff5aa8c16555ca50828afbb7e16dd0bac76a68c9
AhadCove <ahadcove@gmail.com>
2021-09-30 16:15:11 -0500

    fix: port

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import socketio
 
+# Paste token from Lumia Stream (Settings > Advanced > Developers API > Show Token)
 token = 'paste-token-here'
 
 sio = socketio.Client()
+
 
 @sio.event
 def connect():
     print('connection established')
 
+
 @sio.event
 def event(data):
     print('Event', data)
+
 
 @sio.event
 def disconnect(reason):
     print('disconnected from server ', reason)
 
+
 print('Starting socket client')
-sio.connect('http://localhost:39252?token=' + token)
+sio.connect('http://localhost:39231?token=' + token)
 sio.wait()
